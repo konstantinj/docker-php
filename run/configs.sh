d1980310c437cccc8d8a8d9dc3afb5dd1d6eafbd
Konstantin Jakobi <konstantin.jakobi@gmail.com>
2016-03-14 14:11:47 +0100

    added /info.php

--- run/configs.sh
+++ run/configs.sh
@@ -10,8 +10,9 @@ cat /run/php.ini $(php-config --prefix)/etc/all-ext.ini>$(php-config --prefix)/e
 cat /run/php.ini $(php-config --prefix)/etc/all-ext.ini>$(php-config --prefix)/etc/php-no-pthreads.ini
 sed -i -e '/pthreads/d' $(php-config --prefix)/etc/php-no-pthreads.ini
 cat /run/php.ini $(php-config --prefix)/etc/all-ext.ini>$(php-config --prefix)/etc/php-no-xdebug.ini
-sed -i -e '/xdebug/d' $(php-config --prefix)/etc/php-no-pthreads.ini
+sed -i -e '/xdebug/d' $(php-config --prefix)/etc/php-no-xdebug.ini
 cp /run/php.ini $(php-config --prefix)/etc/php.ini
 cp /run/php-fpm.conf $(php-config --prefix)/etc/php-fpm.conf
 :>$(php-config --prefix)/etc/conf.d/dummy.conf
 :>$(php-config --prefix)/etc/php-fpm.d/dummy.conf
+echo "<?php phpinfo();">/usr/share/nginx/html/info.php
